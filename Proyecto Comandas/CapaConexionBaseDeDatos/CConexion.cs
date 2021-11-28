d9987cf03a16a3555381171327d76aa3fd72b246
HectorSauR <hector.sauceda.01@gmail.com>
2021-11-28 12:49:11 -0700

    Inicio

---
+++ Proyecto Comandas/CapaConexionBaseDeDatos/CConexion.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaConexionBaseDeDatos
+{
+    class CConexion
+    {
+        SqlConnection Conexion = new SqlConnection("Server =LAPTOP-NETFLE8Q\\SQLEXPRESS; DataBase = BD_Restaurante; Integrated Security = true");
+        //LAPTOP-NETFLE8Q\\SQLEXPRESS
+        //HOUSE-PC\\SQLEXPRESS
+        //LAPTOP-6F7NE1HE\\SQLEXPRESS
+
+        //Metodos abrir y cerrar conexion.
+        void abrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed) Conexion.Open();
+        }
+
+        void cerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open) Conexion.Close();
+        }
+
+        //Método para ejecutar procedimiento almacenado.
+
+        public void EjectuarPA(string NombrePA, List<CParametros> ListP)
+        {
+            SqlCommand Cm;
+
+            try// Hace un intento para abrir la conexion y en caso de fallo no sea un error fatal
+            {
+                abrirConexion();
+                Cm = new SqlCommand(NombrePA, Conexion);
+                Cm.CommandType = CommandType.StoredProcedure;
+                if (ListP != null)
+                {
+                    for (int i = 0; i < ListP.Count; i++)
+                    {
+                        if (ListP[i].Dir == ParameterDirection.Input)
+                        {
+                            Cm.Parameters.AddWithValue(ListP[i].Nom, ListP[i].Val);
+                        }
+                        if (ListP[i].Dir == ParameterDirection.Output)
+                        {
+                            Cm.Parameters.Add(ListP[i].Nom, ListP[i].TDato, ListP[i].Tam).Direction = ParameterDirection.Output;
+                        }
+
+                    }
+                    Cm.ExecuteNonQuery();
+
+                    //-- Recuperar los parametros de salida -- 
+
+                    for (int i = 0; i < ListP.Count; i++)
+                    {
+                        if (Cm.Parameters[i].Direction == ParameterDirection.Output)
+                        {
+                            ListP[i].Val = Cm.Parameters[i].Value.ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            cerrarConexion();
+        }
+        //-- Método para las consultas en SQL
+        public DataTable Listado(string NombrePA, List<CParametros> ListP)
+        {
+            DataTable DT = new DataTable();
+            SqlDataAdapter DA;
+            try
+            {
+                DA = new SqlDataAdapter(NombrePA, Conexion);
+                DA.SelectCommand.CommandType = CommandType.StoredProcedure;
+                if (ListP != null)
+                {
+                    for (int i = 0; i < ListP.Count; i++)
+                    {
+                        DA.SelectCommand.Parameters.AddWithValue(ListP[i].Nom, ListP[i].Val);
+                    }
+                }
+                DA.Fill(DT);
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+            return DT;
+        }
+    }
+}
---
+++ Proyecto Comandas/CapaConexionBaseDeDatos/CParametros.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaConexionBaseDeDatos
+{
+    class CParametros
+    {
+        //-- ** Atributos de un Parametro
+        private String _Nom;
+        private Object _Val;
+        private SqlDbType _TDato;
+        private Int32 _Tam;
+        private ParameterDirection _Dir;
+
+        //-- ** Propiedades (Caracteristicas que todo Parametro Contiene)
+
+        public string Nom { get => _Nom; set => _Nom = value; }
+        public object Val { get => _Val; set => _Val = value; }
+        public SqlDbType TDato { get => _TDato; set => _TDato = value; }
+        public int Tam { get => _Tam; set => _Tam = value; }
+        public ParameterDirection Dir { get => _Dir; set => _Dir = value; }
+
+        //-- ** Contructores
+
+        // Constructor de Entrada
+        public listParam(string ObjNom, Object ObjVal)
+        {
+            Nom = ObjNom;
+            Val = ObjVal;
+            Dir = ParameterDirection.Input;
+        }
+        // Constructor de salida
+        public listParam(String ObjNom, SqlDbType ObjTDato, Int32 Objtam)
+        {
+            Nom = ObjNom;
+            TDato = ObjTDato;
+            Tam = Objtam;
+            Dir = ParameterDirection.Output;
+        }
+    }
+}
